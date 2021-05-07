ad1aaeddccedad69cdf21fac12a778d49789859d
Rami <ramirofontalva@gmail.com>
2021-05-07 17:04:45 -0300

    Lookup component

--- frontend/src/components/Lookup.js
+++ frontend/src/components/Lookup.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import dbUtils from '../utils/dbUtils';
+import Formula from './Formula';
+import '../index.css';
 
 const Lookup = () => {
-  const [search, setSearch] = React.useState({ title: '', category: '', topic: '' });
+  const [search, setSearch] = React.useState({ title: '', txt: '', equation: '' });
+  const [formulas, setFormulas] = React.useState([]);
+  const getFormulas = () => {
+    const query = `select * from formulapp.equation where title like '%${search.title}%' and txt like '%${search.txt}%' and equation like '%${search.equation}%'`;
+    dbUtils.getRows(query)
+      .then((results) => setFormulas(results))
+      .catch((err) => console.error(err));
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
+    getFormulas();
   };
 
   const changeHandler = (e) => {
@@ -13,25 +25,42 @@ const Lookup = () => {
   };
 
   return (
-    <article className="grid">
-      <div className="lookup-box">
-        <form onSubmit={submitHandler}>
-          <label htmlFor="searchTitle">
-            Title
-            <input type="text" id="searchTitle" name="title" onChange={changeHandler} value={search.title} />
-          </label>
-          <label htmlFor="formulaTxt">
-            Description
-            <textarea id="searchCategory" name="category" onChange={changeHandler} value={search.txt} />
-          </label>
-          <label htmlFor="formulaEquation">
-            Equation
-            <input type="text" id="searchTopic" name="topic" onChange={changeHandler} value={search.topic} />
-          </label>
-        </form>
-        <button type="submit" onClick={submitHandler}>Enviar</button>
+    <>
+      <article className="grid-side">
+        <div className="input-formula">
+          <form onSubmit={submitHandler}>
+            <label htmlFor="searchTitle">
+              Title
+              <input type="text" id="searchTitle" name="title" onChange={changeHandler} value={search.title} />
+            </label>
+            <label htmlFor="searchCategory">
+              Description
+              <textarea id="searchCategory" name="txt" onChange={changeHandler} value={search.txt} />
+            </label>
+            <label htmlFor="searchEquation">
+              Equation
+              <input type="text" id="searchEquation" name="equation" onChange={changeHandler} value={search.equation} />
+            </label>
+          </form>
+          <button type="submit" onClick={submitHandler}>Enviar</button>
+        </div>
+      </article>
+      <div className="formulas-container" style={{ marginTop: '1.6rem' }}>
+        {formulas.map((
+          {
+            id_equation, title, equation, txt,
+          },
+        ) => (
+          <Formula
+            key={id_equation}
+            id={id_equation}
+            title={title}
+            equation={equation}
+            txt={txt}
+          />
+        ))}
       </div>
-    </article>
+    </>
   );
 };
 
